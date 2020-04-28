03f53f20075cb58acef7ce5c6cf4d0539f8a40c3
christoffernissen <christoffer.nissen@gmail.com>
2020-04-28 20:19:45 +0200

    hopefully fixed templates not found

--- template/python3-armhf-org/index.py
+++ template/python3-armhf-org/index.py
@@ -1,5 +1,4 @@
 # Copyright (c) Alex Ellis 2017. All rights reserved.
-# Copyright (c) OpenFaaS Author(s) 2018. All rights reserved.
 # Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 import sys
@@ -19,3 +18,4 @@ if __name__ == "__main__":
     ret = handler.handle(st)
     if ret != None:
         print(ret)
+    
